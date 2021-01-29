0d54c374f2257ca818f758d7784ed82468260d93
KatalystPrime <artinnoyan@gmail.com>
2021-01-29 18:50:09 +0400

    added getdestinyuserid, getmemberidfromlink, and adjusted other methods

--- Catamagne/ExternalAPIs/BungieTools.cs
+++ Catamagne/ExternalAPIs/BungieTools.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BungieSharper.Schema.User;
 
 namespace Catamagne.API
 {
@@ -21,10 +22,10 @@ namespace Catamagne.API
             public ulong? SteamID;
             public string SteamName;
             public ulong? DiscordID;
-            public UserStatus UserStatus;
+            public UserStatus.StatusEnum UserStatus;
             public string[] ExtraColumns;
             public bool? Private;
-            public BungieUser(string BungieNetLink, ulong? BungieNetID, string BungieNetName, string SteamLink, ulong? SteamID, string SteamName, ulong? DiscordID, UserStatus UserStatus = UserStatus.ok, string[] ExtraColumns = null, bool? Private = false)
+            public BungieUser(string BungieNetLink, ulong? BungieNetID, string BungieNetName, string SteamLink, ulong? SteamID, string SteamName, ulong? DiscordID, UserStatus.StatusEnum UserStatus = Models.UserStatus.StatusEnum.ok, string[] ExtraColumns = null, bool? Private = false)
             {
                 this.BungieNetLink = BungieNetLink; this.BungieNetID = BungieNetID; this.BungieNetName = BungieNetName; this.SteamLink = SteamLink; this.SteamID = SteamID; this.SteamName = SteamName; this.DiscordID = DiscordID; this.UserStatus = UserStatus; this.ExtraColumns = ExtraColumns; this.Private = true;
             }
@@ -48,13 +49,13 @@ namespace Catamagne.API
     {
         static BungieApiClient bungieApi = new BungieApiClient(ConfigValues.configValues.BungieAPIKey);
 
-        public async Task<GroupResponse> GetClan(Clan clan)
+        public static async Task<GroupResponse> GetClan(Clan clan)
         {
             var bungieClan = await bungieApi.ApiEndpoints.GroupV2_GetGroup(clan.details.BungieNetID);
             return bungieClan;
         }
 
-        public async Task<(List<GroupMember> publicMembers, List<GroupMember> privateMembers)> GetClanMembers(Clan clan)
+        public static async Task<(List<GroupMember> publicMembers, List<GroupMember> privateMembers)> GetClanMembers(Clan clan)
         {
             SearchResultOfGroupMember group = await bungieApi.ApiEndpoints.GroupV2_GetMembersOfGroup(1, Convert.ToInt64(clan.details.BungieNetID));
             var groupList = group.results.ToList();
@@ -70,10 +71,67 @@ namespace Catamagne.API
 
             return "https://www.bungie.net/en/Profile/" + Convert.ToInt32(membershipType) + "/" + membershipId.ToString();
         }
-        public async Task<Clan> GetClanContainingUser(BungieUser user)
+        public static async Task<Clan> GetClanContainingUser(BungieUser user)
         {
             var _ = Clans.clans.Where(t => t.members.BungieUsers.Contains(user)).FirstOrDefault();
             return _;
+        }
+        public static async Task<long?> GetDestinyUserID(string profileLink)
+        {
+            UserMembershipData user = null;
+            long? _ = GetMemberIDFromLink(profileLink);
+            if (_.HasValue)
+            {
+                long profileID = (long)_;
+                string profileType = "";
+                if (profileLink.Length > 0)
+                {
+                    int numcount = 0;
+                    var profileSplit = profileLink.Split('/');
+                    foreach (var part in profileSplit)
+                    {
+                        int pType;
+                        if (int.TryParse(part, out pType) && numcount == 0)
+                        {
+                            numcount++;
+                            profileType = pType.ToString();
+                        }
+                    }
+                    user = await bungieApi.ApiEndpoints.User_GetMembershipDataById(profileID, Enum.Parse<BungieMembershipType>(profileType));
+                }
+                if (user.bungieNetUser != null)
+                {
+                    return user.bungieNetUser.membershipId;
+                    var a = user.destinyMemberships.FirstOrDefault().membershipId;
+                }
+                else
+                {
+                    return user.destinyMemberships.FirstOrDefault().membershipId;
+                }
+            }
+            return null;
+        }
+        public static long? GetMemberIDFromLink(string profileLink)
+        {
+            if (profileLink.Length > 0)
+            {
+                var _ = profileLink.Split('/');
+                int numCount = 0;
+                foreach (var part in _)
+                {
+                    long numPart;
+                    if (long.TryParse(part, out numPart))
+                    {
+                        numCount++;
+                    }
+                    if (numCount == 2)
+                    {
+                        return Convert.ToInt64(numPart);
+                    }
+                }
+                return null;
+            }
+            return null;
         }
     }
 }
